6af45750f34d348b9d0872159c849050d52c6d1f
Александр Сиденов <sid_san@mail.ru>
2022-02-06 11:31:55 +0800

    Исправление

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int creditAmount = 1_000_000;
-        int years = 3;
-        double percent = 9.99;
-        double result = service.calculate(creditAmount, years, percent);
-        System.out.println("Ежемесячный платеж " + result);
-        System.out.println("Процентная ставка " + percent);
+
+        double result1 = service.calculate(1_000_000, 1, 9.99);
+        System.out.println("Ежемесячный платеж " + result1 + " рублей");
+
+        double result2 = service.calculate(1_000_000, 2, 9.99);
+        System.out.println("Ежемесячный платеж " + result2 + " рублей");
+
+        double result3 = service.calculate(1_000_000, 3, 9.99);
+        System.out.println("Ежемесячный платеж " + result3 + " рублей");
+
     }
 }
